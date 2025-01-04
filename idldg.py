19b3fa40a9e311d3a131b04375316ee3d57adfb6
ctsai64 <168315570+ctsai64@users.noreply.github.com>
2025-01-03 21:05:18 -0500

    Add files via upload

---
+++ idldg.py
@@ -0,0 +1,11 @@
+# All together now
+# By: Cymberly Tsai 12/28/2021
+
+import sys
+sys.path.append("..")
+from detector import detector
+from locater import*
+from distancer import*
+import RPi.GPIO as GPIO
+
+
